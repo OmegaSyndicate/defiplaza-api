ebce20f7a5a59efd4d748c9c3776cd1e4b1f7af0
Timan Rebel <timan@trebel.nl>
2024-04-23 16:14:00 +0200

    chore: test RET on Cloudflare

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,11 @@
+import { NetworkId } from "@radixdlt/radix-engine-toolkit";
+
+export const RADIX_GATEWAY_URL = 'https://mainnet.radixdlt.com'; //'https://gateway-api.astrolescent.com';
+export const RADIX_NETWORK_ID = NetworkId.Mainnet;
+export const XRD_RESOURCE_ADDRESS = 'resource_rdx1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxradxrd';
+export const ACCOUNT_ADDRESS = 'account_rdx16y0nx6uudf05vmw89zyfasf74psv09qjc04pasapeng2d25q675vtd';
+
+
 const headers = {
 	"content-type": "application/json;charset=UTF-8",
 	"Access-Control-Allow-Origin": "*",
